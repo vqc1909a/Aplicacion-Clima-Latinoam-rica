618ee12505bd5a9710dbc56cbb723dfa7bacc08d
Nero Angel <neroangel1909@gmail.com>
2020-09-15 12:12:12 -0500

    Añadiendo el heper

---
+++ src/helpers.js
@@ -0,0 +1,12 @@
+export const paises = [
+          {value: 'PE', pais: 'Perú'},
+          {value: 'AR', pais: 'Argentina'},
+          {value: 'CO', pais: 'Colombia'},
+          {value: 'BO', pais: 'Bolivia'},
+          {value: 'BR', pais: 'Brasil'},
+          {value: 'VE', pais: 'Venezuela'},
+          {value: 'UY', pais: 'Uruguay'},
+          {value: 'PY', pais: 'Paraguay'},
+          {value: 'EC', pais: 'Ecuador'},
+          {value: 'CL', pais: 'Chile'},
+     ]    
